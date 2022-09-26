3f2efcb60e03f1c98ae2dd0ee757a4c22c8b6611
dxyinme <dxyinme@outlook.com>
2022-09-26 21:00:16 +0800

    UPDATE it

--- Async/GetUrl/Program.cs
+++ Async/GetUrl/Program.cs
@@ -1,9 +1,7 @@
 ﻿namespace CSharpLearn.Async
 {
-
     class GetUrl
     {
-
         static async Task<string> GetInfoUrl(string url)
         {
             var client = new HttpClient();
@@ -18,15 +16,18 @@
             var urls = new List<string>{
                 "https://www.baidu.com/",
                 "https://www.bilibili.com/",
-                "https://www.qq.com/",
+                "https://www.sina.com/",
             };
+            var taskList = new List<Task<string>>();
             foreach (var url in urls)
             {
-                var t = GetInfoUrl(url);
-                t.Wait();
-                Console.WriteLine(t.Result.Length);
+                taskList.Add(GetInfoUrl(url));
+            }
+            Task.WhenAll(taskList).Wait();
+            for (int i = 0; i < urls.Count; i++)
+            {
+                Console.WriteLine($"{urls[i]}  length:{taskList[i].Result.Length}");
             }
         }
     }
-
 }
